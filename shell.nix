a5f0a8531e6464a551ca8bb2fbc5a591886bfacc
José Romildo Malaquias <malaquias@gmail.com>
2020-03-16 12:33:38 -0300

    Add nix-shell.nix

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ nixpkgs ? import <nixpkgs> {} } :
+
+let
+  inherit (nixpkgs) pkgs;
+  #ocamlPackages = pkgs.ocamlPackages;
+  ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_07;
+  #ocamlPackages = pkgs.ocamlPackages_latest;
+in
+
+pkgs.stdenv.mkDerivation {
+  name = "my-ocaml-env-0";
+  buildInputs = [
+    ocamlPackages.ocaml
+    ocamlPackages.findlib
+    ocamlPackages.camomile
+    ocamlPackages.merlin
+    ocamlPackages.menhir
+    pkgs.dune
+    pkgs.emacs
+    pkgs.vscode
+    pkgs.rlwrap
+  ];
+}
